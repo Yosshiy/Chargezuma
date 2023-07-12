3ecf4b637744f0708cc12bdc40f1bfc8de94a766
MSI\Owner <ore329@icloud.com>
2023-07-13 02:08:30 +0900

    新規敵キャラ3Dモデル追加

--- Assets/CharaMove.cs
+++ Assets/CharaMove.cs
@@ -1,3 +1,4 @@
+﻿using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UniRx;
@@ -7,11 +8,11 @@ public class CharaMove : MonoBehaviour
 {
     Rigidbody rb;
     float JumpPower;
-    
+    float time;
+    float exittime;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
         Observable.EveryUpdate()
                   .Where(x => Input.GetAxis("Horizontal") != 0)
                   .Subscribe(x => OnMove());
@@ -30,20 +31,31 @@ public class CharaMove : MonoBehaviour
 
     private void OnMove()
     {
-        var Direction = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
-        rb.AddForce(Direction * 7);
+        var input = new Vector3(Input.GetAxis("Horizontal") * 15, 0, Input.GetAxis("Vertical") * 15);
+        //　移動速度計算
+        input.Normalize();
+        input *= 10;
+        input.y = rb.velocity.y;
+        //　移動処理
+        rb.velocity = input;
     }
 
     void Jump()
     {
         var Direction = new Vector3(0, 2, 0);
         rb.AddForce(Direction * 2,ForceMode.Impulse);
+        time = Time.time;
     }
     
     void JumpA()
     {
-        var Direction = new Vector3(0, 5, 0);
-        rb.AddForce(Direction * 2,ForceMode.Impulse);
+        exittime = Time.time - time;
+        if(exittime > 1)
+        {
+            exittime = 1;
+        }
+        var Direction = new Vector3(0, exittime, 0);
+        rb.AddForce(Direction * 17,ForceMode.Impulse);
     }
 
 
